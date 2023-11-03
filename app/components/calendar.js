32a42ec9f254245bb19bac21d853a7926ed6f2ca
Kengo <83492993+KengoKato02@users.noreply.github.com>
2023-11-03 11:07:07 +0100

    calendar fix & dropdowns

--- app/components/calendar.js
+++ app/components/calendar.js
@@ -1,3 +1,12 @@
 import Component from '@glimmer/component';
+import {tracked} from '@glimmer/tracking';
+import {action} from '@ember/object';
 
-export default class CalendarComponent extends Component {}
+export default class CalendarComponent extends Component {
+ @tracked isOpen = false;
+
+ @action
+ toggleMonthDropDown() {
+  this.isOpen = !this.isOpen;
+ }
+}
