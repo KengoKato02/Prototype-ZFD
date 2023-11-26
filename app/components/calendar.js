78ecb47955249139d2961e90acebdeed94f1cd7d
Kengo <83492993+KengoKato02@users.noreply.github.com>
2023-11-26 17:15:25 +0100

    real-time calendar

--- app/components/calendar.js
+++ app/components/calendar.js
@@ -26,11 +26,121 @@ export default class CalendarComponent extends Component {
   );
 
   @tracked endOfMonthDate = endOfMonth(new Date(this.currentYear, this.currentMonth - 1, 1));
+  @tracked startOfMonthDate = startOfMonth(new Date(this.currentYear, this.currentMonth - 1, 1));
 
-  @tracked daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
+  @tracked dayLabels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
   
 
   @tracked currentWeek = getWeekOfMonth(new Date(this.currentYear, this.currentMonth - 1, this.currentDay));
+
+  
+  get calcWeeks() {
+    const startDate = new Date(this.startOfMonthDate);
+    const startDay = startDate.getDay();
+    const daysInPreviousMonth = getDaysInMonth(new Date(this.currentYear, this.currentMonth - 2, 1));
+    const weeks = [];
+  
+    let currentWeek = [];
+  
+    // Fill the first week with the days from the previous month
+    for (let i = 0; i < startDay; i++) {
+      currentWeek.push(daysInPreviousMonth - (startDay - i) + 1);
+    }
+  
+    // Fill the remaining days of the first week with the current month
+    const totalDaysInMonth = getDaysInMonth(new Date(this.currentYear, this.currentMonth - 1, 1));
+    for (let i = 0; i < 7 - startDay; i++) {
+      currentWeek.push(i + 1);
+    }
+    weeks.push(currentWeek);
+  
+    let remainingDays = totalDaysInMonth - (7 - startDay);
+    let week = 1;
+    let nextMonth = this.currentMonth + 1;
+    let nextYear = this.currentYear;
+    let totalDaysInNextMonth = getDaysInMonth(new Date(nextYear, nextMonth - 1, 1));
+  
+    // Continue filling weeks with the current month
+    while (remainingDays > 0) {
+      currentWeek = [];
+      for (let i = 0; i < 7; i++) {
+        const day = week * 7 + i - startDay + 1;
+        if (day <= totalDaysInMonth) {
+          currentWeek.push(day);
+          remainingDays--;
+        } else {
+          currentWeek.push(day - totalDaysInMonth);
+        }
+      }
+      weeks.push(currentWeek);
+      week++;
+    }
+  
+    return weeks;
+  }
+  
+  get firstWeek() {
+    const calcWeeksResult = this.calcWeeks;
+    if (calcWeeksResult.length > 0) {
+      const firstWeek = calcWeeksResult[0].map((day, index) => {
+        const dayLabel = this.dayLabels[index];
+        return { dayNumber: day, dayLabel };
+      });
+      return firstWeek;
+    } else {
+      return [];
+    }
+  }
+  
+  get secondWeek(){
+    const calcWeeksResult = this.calcWeeks;
+    if (calcWeeksResult.length > 0) {
+      const secondWeek = calcWeeksResult[1].map((day, index) => {
+        const dayLabel = this.dayLabels[index];
+        return { dayNumber: day, dayLabel };
+      });
+      return secondWeek;
+    } else {
+      return [];
+    }
+  }
+  get thirdWeek(){
+    const calcWeeksResult = this.calcWeeks;
+    if (calcWeeksResult.length > 0) {
+      const thirdWeek = calcWeeksResult[2].map((day, index) => {
+        const dayLabel = this.dayLabels[index];
+        return { dayNumber: day, dayLabel };
+      });
+      return thirdWeek;
+    } else {
+      return [];
+    }
+  }
+  get fourthWeek(){
+    const calcWeeksResult = this.calcWeeks;
+    if (calcWeeksResult.length > 0) {
+      const fourthWeek = calcWeeksResult[3].map((day, index) => {
+        const dayLabel = this.dayLabels[index];
+        return { dayNumber: day, dayLabel };
+      });
+      return fourthWeek;
+    } else {
+      return [];
+    }
+  }
+  get fifthWeek(){
+    const calcWeeksResult = this.calcWeeks;
+    if (calcWeeksResult.length > 0) {
+      const fifthWeek = calcWeeksResult[0].map((day, index) => {
+        const dayLabel = this.dayLabels[index];
+        return { dayNumber: day, dayLabel };
+      });
+      return fifthWeek;
+    } else {
+      return [];
+    }
+  }
+  
 
   @action
   toggleBookHoliday() {
@@ -100,29 +210,29 @@ export default class CalendarComponent extends Component {
   get weeksInMonth() {
     const startOfMonthDate = startOfMonth(new Date(this.currentYear, this.currentMonth - 1, 1));
     const endOfMonthDate = endOfMonth(new Date(this.currentYear, this.currentMonth - 1, 1));
-    let currentWeek = [];
+
+    let currentDay = 1; // Start from the 1st day of the month
     const weeks = [];
 
-    // Push all days of the month into currentWeek array
-    for (let currentDate = startOfMonthDate; currentDate <= endOfMonthDate; currentDate = addDays(currentDate, 1)) {
-      currentWeek.push(format(currentDate, 'd'));
-
-      if (currentDate.getDay() === 6) {
-        weeks.push([...currentWeek]);
-        currentWeek = [];
-      }
-    }
-
-    // Populate weeks array with days of the month
-    let currentDay = 0;
-    for (let i = 0; i < weeks.length; i++) {
-      for (let j = 0; j < weeks[i].length; j++) {
-        weeks[i][j] = currentDay < weeks[i].length ? currentDay + 1 : '';
-        currentDay++;
-      }
-    }
-
     
+    while (currentDay <= endOfMonthDate.getDate()) {
+      const currentWeek = [];
+      
+      // Fill the current week with days of the month
+      for (let i = 0; i < 7; i++) {
+        const currentDate = new Date(this.currentYear, this.currentMonth - 1, currentDay);
+
+        // Check if the current date is still within the month
+        if (currentDate <= endOfMonthDate) {
+          currentWeek.push(format(currentDate, 'd'));
+          currentDay++;
+        } else {
+          currentWeek.push('');
+        }
+      }
+
+      weeks.push([...currentWeek]);
+    }
 
     return weeks;
   }
